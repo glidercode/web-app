49aae077d1d715191b7a7c2691f98379b6f6d8b4
glidercode <57229380+glidercode@users.noreply.github.com>
2022-01-31 17:16:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,12 +44,11 @@ app.use(
   authRequired: false,
   auth0Logout: true,
   baseURL: APP_URL,
-  // 👇 add this 👇
   authorizationParams: {
    response_type: "code id_token",
    audience: "https://expenses-api",
+   scope: "openid profile email read:reports",
   },
-  // 👆 add this 👆
  })
 );
 
@@ -79,15 +78,12 @@ app.get("/user",requiresAuth(), async (req, res) => {
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
  try {
-  // 👇 get the token from the request 👇
   const { token_type, access_token } = req.oidc.accessToken;
-  // 👇 then send it as an authorization header 👇
   const expenses = await axios.get(`${API_URL}/reports`, {
    headers: {
     Authorization: `${token_type} ${access_token}`,
    },
   });
-  // 👆 end of changes 👆
   res.render("expenses", {
    user: req.oidc && req.oidc.user,
    expenses: expenses.data,
